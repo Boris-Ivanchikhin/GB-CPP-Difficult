909da2e8eec9e5f9ad98419233b8ba30bacc0729
Boris Ivanchikhin <bob_mail@mail.ru>
2023-01-25 16:10:37 +0300

    lesson 07

---
+++ 07/Lesson_07_Namespace_and_binary_serialization.cpp
@@ -0,0 +1,619 @@
+// *** Урок 07.  Пространство имен и бинарная серилизация / Namespace and binary serialization
+
+#include <locale.h>  // Для setlocale (LC_ALL, "")
+#include <iostream>
+
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <optional>
+#include <algorithm>
+
+#include "Exercise-02.pb.h"
+
+using namespace std;
+
+/*
+ * 1. Установить библиотеку protobuf.
+ *    Protocol Buffers - Google's data interchange format.
+ */
+
+// См.файл «SetUp_Google_protobuf_in_MSYS2.pdf»
+
+
+/*
+ * 2. С помощью компилятора protobuf в отдельном пространстве имен сгенерировать классы:
+ *      a. FullName с полями имя, фамилия, отчество (отчество опционально);
+ *      b. Student с полями полное имя, массив оценок, средний балл;
+ *      c. StudentsGroup с полем массив студентов.
+ */
+
+// См.файл «Exercise-02.proto»
+// См.файл «Exercise-02.pb.h»
+// См.файл «Exercise-02.pb.cc»
+
+namespace Exercise_2 {
+
+    const string f_name {"Exercise-02.dat"};
+
+    // This function fills in a Student message based on user input.
+    void PromptForStudent (lesson07::Student* student) {
+
+        cin.ignore(256, '\n');
+
+        // message FullName
+        auto *full_name =  student->mutable_fio();
+
+        cout << "\nenter first name: ";
+        getline (cin, *full_name->mutable_firstname());
+
+        cout << "enter last name: ";
+        getline (cin, *full_name->mutable_lastname());
+
+        cout << "enter second name: ";
+        getline (cin, *full_name->mutable_patronymic());
+
+        float _total {0.0};
+        while (true) {
+            cout << "enter a grade (or leave blank to finish): ";
+            string _str_value;
+            getline (cin, _str_value);
+            if (_str_value.empty ()) {
+                break;
+            }
+            stringstream _ss {_str_value};
+            int _number;
+            _ss >> _number;
+
+            student->add_grades (_number);
+            _total += static_cast <float> (_number);
+        }
+
+        student->set_avggrade (_total / static_cast <float> (student->grades_size()));
+
+    } // PromptForStudent
+
+    // Iterates though all students in the StudentsGroup and prints info about them.
+    void ListStudents (const lesson07::StudentsGroup &students_group) {
+        for (int i = 0; i < students_group.students_size(); i++) {
+            const lesson07::Student& student = students_group.students (i);
+
+            cout << "\n#" << i + 1 << endl;
+            cout << "first name: " << student.fio().firstname() << endl;
+            cout << "last name: " << student.fio().lastname() << endl;
+
+            if (student.fio().has_patronymic())
+                cout << "second name: " << student.fio().patronymic() << endl;
+
+            cout << "avg.grade: " << student.avggrade() << endl;
+
+            cout << "grades: ";
+            for (int j = 0; j < student.grades_size (); j++)
+                cout << student.grades (j) << " ";
+            cout << endl;
+        }
+    } // ListStudents
+
+} // namespace Exercise_2
+
+/*
+ * 3. Создать класс StudentsGroup, который реализует интерфейсы:
+ *
+ *      class IRepository {
+ *          virtual void Open() = 0; // бинарная десериализация из файла
+ *          virtual void Save() = 0; // бинарная сериализация в файл
+ *      };
+ * 
+ *      class IMethods {
+ *          virtual double GetAverageScore (const FullName& name) = 0;
+ *          virtual string GetAllInfo (const FullName& name) = 0;
+ *          virtual string GetAllInfo () = 0;
+ *      };
+ * 
+ */
+
+namespace Exercise_3 {
+
+    const string f_name {"Exercise-03.dat"};
+
+    struct FullName;
+
+    // * class IRepository
+
+    class IRepository
+    {
+        public:
+            // binary deserialization from a file
+            virtual auto Open (ifstream &) -> size_t = 0;
+            // binary serialization to a file
+            virtual auto Save (ofstream &) -> size_t = 0; 
+
+            IRepository() = default;
+            ~IRepository() = default;
+    }; // class IRepository
+
+    // * class IMethods
+
+    class IMethods
+    {
+        public:
+            virtual optional <double> GetAverageScore (const FullName &) = 0;
+            virtual optional <string> GetAllInfo (const FullName &) = 0;
+            virtual string GetAllInfo () = 0;
+
+            IMethods() = default;
+            ~IMethods() = default;
+    }; // class IMethods
+
+    // * class FullName 
+
+    class FullName : public IRepository
+    {
+        public :
+            string firstName;
+            string lastName;
+            string patronymic;
+
+            // interface IRepository
+            auto Open (ifstream &) -> size_t override; // binary deserialization from a file
+            auto Save (ofstream &) -> size_t override; // binary serialization to a file
+
+            FullName (const string& _firstName, const string& _lastName,const string& _patronymic)
+            : firstName (_firstName), lastName (_lastName) , patronymic (_patronymic)
+            {};
+            FullName ()
+            : firstName (""), lastName ("") , patronymic ("")
+            {};
+            ~FullName () = default;
+
+    }; // class FullName
+
+    auto FullName::Open (ifstream &in)  -> size_t
+    {
+        const auto pos = in.tellg ();
+        size_t sz{};
+        char *buf;
+
+        // firstName
+        in.read (reinterpret_cast <char*> (&sz), sizeof (sz));
+        firstName.clear();
+        if (sz) {
+            buf = new char [sz];
+            in.read (buf, sz);
+            firstName.append (buf, 0, sz);
+            //firstName.resize (sz);
+            delete []buf;
+        }
+
+        // lastName
+        lastName.clear();
+        in.read (reinterpret_cast <char*> (&sz), sizeof (sz));
+        if (sz) { 
+            buf = new char [sz];
+            in.read (buf, sz);
+            lastName.append (buf, 0, sz);
+            //lastName.resize (sz);
+            delete []buf;
+        }
+
+        // patronymic
+        patronymic.clear();
+        in.read (reinterpret_cast <char*> (&sz), sizeof (sz));
+        if (sz) {
+            buf = new char [sz];
+            in.read (buf, sz);
+            patronymic.append (buf, 0, sz);
+            //patronymic.resize (sz);
+            delete []buf;
+        }
+
+        // return
+        return static_cast <ssize_t> (in.tellg () - pos);
+
+    } // FullName::Open
+
+    auto FullName::Save (ofstream &out) -> size_t
+    {
+        const auto pos = out.tellp ();
+
+        // firstName
+        size_t sz = firstName.length ();
+        out.write (reinterpret_cast <const char*> (&sz), sizeof (sz));
+        out.write (firstName.c_str(), firstName.length());
+        
+        // lastName
+        sz = lastName.length ();
+        out.write (reinterpret_cast <const char*> (&sz), sizeof (sz));
+        out.write (lastName.c_str (), lastName.length());
+        
+        // patronymic
+        sz = patronymic.length ();
+        out.write (reinterpret_cast <const char*> (&sz), sizeof (sz));
+        out.write (patronymic.c_str (), patronymic.length());
+
+        // return
+        return static_cast <ssize_t> (out.tellp () - pos);
+
+    } // FullName::Save
+
+    // * class Student
+    
+    class Student : public IRepository
+    {
+        public:
+            FullName FIO;
+            vector <uint16_t> grades;
+            float avgGrade;
+
+            // interface IRepository
+            auto Open (ifstream &) -> size_t override; // binary deserialization from a file
+            auto Save (ofstream &) -> size_t override; // binary serialization to a file
+
+            Student() : avgGrade (0.0f) {};
+            ~Student() = default;
+
+    }; // class Student
+
+    auto Student::Open (ifstream &in) -> size_t
+    {
+        const auto pos = in.tellg ();
+
+        // FIO
+        FIO.Open (in);
+
+        // avgGrade
+        in.read (reinterpret_cast <char*> (&avgGrade), sizeof (avgGrade));
+
+        // grades (vector <uint16_t>)
+        grades.clear();
+        grades.shrink_to_fit();
+        size_t sz{};
+        in.read (reinterpret_cast <char*> (&sz), sizeof (sz));
+        grades.resize (sz);
+        in.read (reinterpret_cast <char*> (&grades[0]), sizeof (uint16_t) * sz);
+
+        // return
+        return static_cast <ssize_t> (in.tellg () - pos);
+
+    } // Student::Open
+
+    auto Student::Save (ofstream &out) -> size_t
+    {
+        const auto pos = out.tellp ();
+
+        // FIO
+        FIO.Save (out);
+
+        // avgGrade
+        out.write (reinterpret_cast <const char*> (&avgGrade), sizeof (avgGrade));
+
+        // grades (vector <uint16_t>)
+        size_t sz = grades.size ();
+        out.write (reinterpret_cast <const char*> (&sz), sizeof (sz));
+        out.write (reinterpret_cast <const char*> (&grades[0]), sizeof (uint16_t) * sz);
+
+        // return
+        return static_cast <ssize_t> (out.tellp () - pos);
+
+    } // Student::Save
+
+    // * class StudentsGroup
+
+    class StudentsGroup : public IRepository, public IMethods
+    {
+        public:
+            // interface IRepository
+            auto Open (ifstream &) -> size_t override; // binary deserialization from a file
+            auto Save (ofstream &) -> size_t override; // binary serialization to a file
+            // interface IRepository
+            optional <double> GetAverageScore (const FullName& name) override;
+            optional <string> GetAllInfo (const FullName& name) override;
+            string GetAllInfo () override;
+
+            void addStudent (Student* _student) {
+                if (_student != nullptr)
+                    students.emplace_back (*_student);
+            };
+
+            StudentsGroup () = default;
+            ~StudentsGroup() = default;
+
+        private:
+            vector <Student> students;
+
+    }; // class StudentsGroup
+
+    auto StudentsGroup::Open (ifstream &in) -> size_t
+    {
+        const auto pos = in.tellg ();
+
+        // students (vector <Student>)
+        students.clear();
+        students.shrink_to_fit();
+        size_t sz{};
+        in.read (reinterpret_cast <char*> (&sz), sizeof (sz));
+        for (size_t i = 0; i < sz; ++i)
+        {
+            Student iobj;
+            iobj.Open (in);
+            students.emplace_back (iobj);
+        }
+
+        // return
+        return static_cast <ssize_t> (in.tellg () - pos);
+
+    } // StudentsGroup::Open
+
+    auto StudentsGroup::Save (ofstream &out) -> size_t
+    {
+        const auto pos = out.tellp ();
+
+        // students (vector <Student>)
+        size_t sz = students.size ();
+        out.write (reinterpret_cast <const char*> (&sz), sizeof (sz));
+        for (size_t i = 0; i < sz; ++i) {
+            auto& iobj = students [i];
+            iobj.Save (out);
+        }
+
+        // return
+        return static_cast <ssize_t> (out.tellp () - pos);
+
+    } // StudentsGroup::Save
+
+    optional <double> StudentsGroup::GetAverageScore (const FullName& name) {
+        auto it = find_if (students.begin(), students.end(),
+                    [&] (const auto& value) {
+                        return (tie (value.FIO.firstName, value.FIO.lastName, value.FIO.patronymic) ==
+                                tie (name.firstName, name.lastName, name.patronymic));
+                    });
+
+        if (it == students.end())
+            return nullopt; // {}
+
+        // return
+        return it->avgGrade;
+
+    } // StudentsGroup::GetAverageScore
+
+    optional <string> StudentsGroup::GetAllInfo (const FullName& name) {
+        
+        auto it = find_if (students.begin(), students.end(),
+                    [&] (const auto& value) {
+                        return (tie (value.FIO.firstName, value.FIO.lastName, value.FIO.patronymic) ==
+                                tie (name.firstName, name.lastName, name.patronymic));
+                    });
+
+        if (it == students.end())
+            return nullopt; // {}
+
+        stringstream _ss {};
+
+        //_ss << "\n#" << i + 1 << endl;
+        _ss << "first name: " << it->FIO.firstName << endl;
+        _ss << "last name: "  << it->FIO.lastName << endl;
+
+        if (!it->FIO.patronymic.empty())
+            _ss << "second name: " << it->FIO.patronymic << endl;
+
+        _ss << "avg.grade: " << it->avgGrade << endl;
+
+        _ss << "grades: ";
+        for (size_t j = 0; j < it->grades.size (); ++j)
+            _ss << it->grades [j] << " ";
+        _ss << endl;
+
+        // return
+        return _ss.str();
+
+    } // StudentsGroup::GetAllInfo
+
+    string StudentsGroup::GetAllInfo () {
+
+        stringstream _ss;
+        int count {};
+
+        for (size_t i = 0; i < students.size(); ++i) {
+            auto info = GetAllInfo (students [i].FIO);
+            if (!info.has_value ())
+                continue;
+            _ss << "\n#" << count + 1 << endl;
+            _ss << info.value ();
+        }
+            
+        // return
+        return _ss.str();
+
+    } // StudentsGroup::GetAllInfo
+
+    // This function fills in a Student object based on user input.
+    void PromptForStudent (Student* student) {
+
+        cin.ignore(256, '\n');
+
+        // class FullName
+        auto &full_name =  student->FIO;
+
+        cout << "\nenter first name: ";
+        getline (cin, full_name.firstName);
+
+        cout << "enter last name: ";
+        getline (cin, full_name.lastName);
+
+        cout << "enter second name: ";
+        getline (cin, full_name.patronymic);
+
+        // grades (vector <uint16_t>)
+        float _total{};
+        while (true) {
+            cout << "enter a grade (or leave blank to finish): ";
+            string _str_value;
+            getline (cin, _str_value);
+            if (_str_value.empty ()) {
+                break;
+            }
+            stringstream _ss {_str_value};
+            int _number;
+            _ss >> _number;
+
+            student->grades.emplace_back (static_cast <uint16_t> (_number));
+            _total += static_cast <float> (_number);
+        }
+
+        student->avgGrade = _total / static_cast <float> (student->grades.size ());
+
+    } // PromptForStudent
+
+} // namespace Exercise_3
+
+
+// Function called at shutdown
+void endFunction (void)
+{
+    // Output of the shutdown message
+    puts ("\nthe work is completed!");
+} // endFunction
+
+// main
+int main (void) {
+
+    // Russian language in the console
+    setlocale (LC_ALL, "");
+
+     // Registration of the function to be called during normal shutdown
+    if (atexit (endFunction)) {
+        // Checking the registration of the endFunction
+        puts ("post- function registration error!");
+    }
+
+    cout << "Lesson 07. Namespace and binary serialization.\n\n";
+
+    // * Exercises #1
+    cout << "Exercise #1.\n";
+    cout << "see the explanations in file «SetUp_Google_protobuf_in_MSYS2.pdf»" << endl;
+
+    // * Exercise #2
+    cout << "\nExercise #2.\n";
+
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    
+    lesson07::StudentsGroup proto_students_group;
+
+    // serialize
+    cout << "\nproceed to serialize?(Y/N): ";
+    char answer = getchar ();
+    if (answer == 'Y' || answer == 'y') {
+        fstream out;
+        out.open (Exercise_2::f_name, ios::out | ios::trunc | ios::binary);
+        if (!out.is_open ()) {
+            cout << "failed to open «" << Exercise_2::f_name << "»!" << endl;
+            return EXIT_FAILURE;
+        }
+        // adding students to a group
+        do {
+            Exercise_2::PromptForStudent (proto_students_group.add_students());
+            cout << "proceed the next?(Y/N): ";
+            answer = getchar ();
+            if (answer != 'Y' && answer != 'y')
+                break;
+        } while (true);
+        // write to the file
+        if (!proto_students_group.SerializeToOstream (&out)) {
+            cout << "failed to write «"<< Exercise_2::f_name << "»!" << endl;
+            //return EXIT_FAILURE;
+        } else {
+            cout << "file «" << Exercise_2::f_name << "» was successfully written." << endl;
+        }
+        // close
+        if (out.is_open ())
+            out.close ();
+    }
+
+    // deserialize
+    cin.ignore(256, '\n');
+    cout << "\nproceed to deserialize?(Y/N): ";
+    answer = getchar ();
+    if (answer == 'Y' || answer == 'y') {
+        fstream in;
+        in.open (Exercise_2::f_name, ios::in | ios::binary);
+        if (!in.is_open ()) {
+            cout << "failed to open «" << Exercise_2::f_name << "»!" << endl;
+            //return EXIT_FAILURE;
+        } else {
+            if (!proto_students_group.ParseFromIstream(&in)) {
+                cout << "failed to parse from «"<< Exercise_2::f_name << "»!" << endl;
+                //return EXIT_FAILURE;
+            } else
+                Exercise_2::ListStudents (proto_students_group);
+        }
+        // close
+        if (in.is_open ())
+            in.close ();
+    }
+    
+    // Optional: Delete all global objects allocated by libprotobuf.
+    google::protobuf::ShutdownProtobufLibrary ();
+
+    // * Exercise #3
+    cout << "\nExercise #3.\n";
+
+    Exercise_3::StudentsGroup obj_students_group;
+
+    // serialize
+    cin.ignore(256, '\n');
+    cout << "\nproceed to serialize?(Y/N): ";
+    answer = getchar ();
+    if (answer == 'Y' || answer == 'y') {
+        ofstream out;
+        out.open (Exercise_3::f_name, ios::out | ios::trunc | ios::binary);
+        if (!out.is_open ()) {
+            cout << "failed to open «" << Exercise_3::f_name << "»!" << endl;
+            return EXIT_FAILURE;
+        }
+        // adding students to a group
+        do {
+            Exercise_3::Student _student{};
+            Exercise_3::PromptForStudent (&_student);
+            obj_students_group.addStudent (&_student);
+            cout << "proceed the next?(Y/N): ";
+            answer = getchar ();
+            if (answer != 'Y' && answer != 'y')
+                break;
+        } while (true);
+        // write to the file
+        if (!obj_students_group.Save (out)) {
+            cout << "failed to write «"<< Exercise_3::f_name << "»!" << endl;
+            //return EXIT_FAILURE;
+        } else {
+            cout << "file «" << Exercise_3::f_name << "» was successfully written." << endl;
+        }
+        // close
+        if (out.is_open ())
+            out.close ();
+    }
+
+    // deserialize
+    cin.ignore(256, '\n');
+    cout << "\nproceed to deserialize?(Y/N): ";
+    answer = getchar ();
+    if (answer == 'Y' || answer == 'y') {
+        ifstream in;
+        in.open (Exercise_3::f_name, ios::in | ios::binary);
+        if (!in.is_open ()) {
+            cout << "failed to open «" << Exercise_3::f_name << "»!" << endl;
+            //return EXIT_FAILURE;
+        } else {
+            if (!obj_students_group.Open (in)) {
+                cout << "failed to parse from «"<< Exercise_3::f_name << "»!" << endl;
+                //return EXIT_FAILURE;
+            } else
+                cout << obj_students_group.GetAllInfo ();
+        }
+        // close
+        if (in.is_open ())
+            in.close ();
+    }
+
+    // return o.k.
+    return EXIT_SUCCESS;
+} // main
--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 #### C++: сложные моменты / C++: difficult moments  
+#### Урок 07. Пространство имен и бинарная сериализация / Namespace and binary serialization  
 
 ***
 
-source code in c++
+1. Установить библиотеку [protobuf](https://developers.google.com/protocol-buffers?hl=ru).  
+
+2. С помощью компилятора <b>protobuf</b> в отдельном пространстве имен сгенерировать классы:  
+    a. <b>FullName</b> с полями имя, фамилия, отчество (отчество опционально);  
+    b. <b>Student</b> с полями полное имя, массив оценок, средний балл;  
+    c. <b>StudentsGroup</b> с полем массив студентов.  
+
+3. Создать класс <b>StudentsGroup</b>, который реализует интерфейсы:  
+
+```C++
+class IRepository {
+    virtual void Open () = 0; // бинарная десериализация в файл
+    virtual void Save () = 0; // бинарная сериализация в файл
+};
+
+class IMethods {
+    virtual double GetAverageScore (const FullName& name) = 0;
+    virtual string GetAllInfo (const FullName& name) = 0;
+    virtual string GetAllInfo () = 0;
+};
+```
